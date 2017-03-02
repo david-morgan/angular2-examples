d561c3bbd9eb93f004c1e97f327f0dd931bf97e3
David Morgan <david.morgan@entelect.co.za>
2017-03-02 09:42:02 +0200

    added angular exmples initial commit

---
+++ angular-tour-of-heroes/src/app/hero.js
@@ -0,0 +1,11 @@
+"use strict";
+/**
+ * Created by david.morgan on 2017/03/01.
+ */
+var Hero = (function () {
+    function Hero() {
+    }
+    return Hero;
+}());
+exports.Hero = Hero;
+//# sourceMappingURL=hero.js.map
---
+++ angular-tour-of-heroes/src/app/hero.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by david.morgan on 2017/03/01.
+ */
+export class Hero {
+  id: number;
+  name: string;
+}
